e454d57b4705a58fbea779a484640cf928dd97d2
whitedog0980 <whitedog0980@gmail.com>
2023-05-24 11:36:09 +0900

    123

--- for_study/pre.js
+++ for_study/pre.js
@@ -1,46 +1,9 @@
-class UserStorage{
-  loginUser(id, password, onSuccess, onError){
-    setTimeout(() =>{
-      if(
-          (id === 'seul' && password === '123') ||
-          (id === 'kim' && password === '456')
-      ) {
-          onSuccess(id);
-      } else{
-          onError(new Error('error'));
-      }
-      }, 2000);
-  }
-  
-  getRoles(user, onSuccess, onError){
-      setTimeout(()=> {
-          if (user === 'seul') {
-              onSuccess({name: 'seul', role: 'admin'});
-          } else {
-              onError(new Error('error'));
-          }
-      }, 1000);
-  }
-};
+function setCookie(name, value, days) {
+  // 만료 시간 설정
+  const expires = new Date();
+  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000); // 현재 시각으로 부터 24시간 후
+  // 쿠키 생성
+  const cookie = `${name}=${encodeURIComponent(value)};expires=${expires.toUTCString()};path=/`;
+  document.cookie = cookie;
+}
 
-
-const userStorage = new UserStorage();
-const id = prompt('아이디를 입력해 주세요!');
-const password = prompt('비밀번호를 입력해 주세요!!');
-
-userStorage.loginUser(
-    id,
-    password,
-    user => {
-        userStorage.getRoles(
-            user,
-            userWithRole => {
-                alert(`hello ${userWithRole.name}, you have a ${userWithRole.role} role`)
-            },
-            error => {
-                console.log('에러2')
-            }
-            );
-        },
-    error => {console.log('에러1')}
-);
